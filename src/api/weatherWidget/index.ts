62921222486ea0ac077fb098fc5e04fbe1cc1d52
Andrey <KharchenkoAndru@gmail.com>
2023-08-14 23:16:34 +0300

    Add API_KEY to Utils

--- src/api/weatherWidget/index.ts
+++ src/api/weatherWidget/index.ts
@@ -1,7 +1,5 @@
+import { API_KEY } from "@/utils/utils";
 import axios from "axios";
-
-const API_KEY = '84882f276cf7ddc264d2de3e25f7cf03';
-//const API_KEY = '7a027cf18a44aa38ea7a7ced214e31bd';
 
 export const instance = axios.create({
     baseURL: 'http://api.openweathermap.org/',
